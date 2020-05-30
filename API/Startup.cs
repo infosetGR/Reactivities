b42286b70596d759a4c8ba6fab08684b96075732
Fotis Stathopoulos <fotisss@gmail.com>
2020-05-30 22:45:57 +0300

    end of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -30,8 +30,19 @@ namespace API
             services.AddDbContext<DataContext>( opt => {
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             }
-            
             );
+
+            services.AddCors(opt => 
+            {
+                opt.AddPolicy("CorsPolicy", policy => 
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
+            });
+            // services.AddMediatR(typeof(List.Handler).Assembly);
+            // services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+
             services.AddControllers();
         }
 
@@ -48,6 +59,8 @@ namespace API
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseCors("CorsPolicy");
 
             app.UseEndpoints(endpoints =>
             {
